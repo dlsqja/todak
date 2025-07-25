e7de2871a8b6e9c01437dfda76918f02829e6d22
송인범 <dlsqja3436@gmail.com>
2025-07-25 16:22:45 +0900

    add: elastic_search 배포용 설정 세팅

--- backend/src/main/java/com/A409/backend/global/config/ElasticsearchConfig.java
+++ backend/src/main/java/com/A409/backend/global/config/ElasticsearchConfig.java
@@ -1,5 +1,6 @@
 package com.A409.backend.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -9,10 +10,13 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "com.A409.backend.global.util.elasticsearch.repository")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUris;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(elasticsearchUris.replace("http://", "")) // protocol 제거
                 .build();
     }
 }
