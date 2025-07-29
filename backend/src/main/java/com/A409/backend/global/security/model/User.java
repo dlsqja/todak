94fbfb28f2fc7113372666cd43311b31cc102d39
송인범 <dlsqja3436@gmail.com>
2025-07-29 09:31:25 +0900

    add: security add 및 UserDetails추가

---
+++ backend/src/main/java/com/A409/backend/global/security/model/User.java
@@ -0,0 +1,40 @@
+package com.A409.backend.global.security.model;
+
+import com.A409.backend.global.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    private String username;
+    private String password;
+    private Long id;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
