6a8ddbdd1b98ad09b77fa87e3c17891bb5169345
송인범 <dlsqja3436@gmail.com>
2025-07-29 17:40:15 +0900

    add : 병원 찾기 관련 레포지 등록

---
+++ backend/src/main/java/com/A409/backend/domain/hospital/service/HospitalService.java
@@ -0,0 +1,14 @@
+package com.A409.backend.domain.hospital.service;
+
+import com.A409.backend.domain.hospital.repository.HospitalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HospitalService {
+
+    private final HospitalRepository hospitalRepository;
+
+    public
+}
