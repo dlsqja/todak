7cc0183c437fe0353b36c4564251cc65f53737a2
daeyeonlee <angellee98@naver.com>
2025-07-29 17:59:50 +0900

    feat : 색상, 글자크기 컴포넌트 추가가

--- frontend/src/pages/Owner/OwnerHome.tsx
+++ frontend/src/pages/Owner/OwnerHome.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '@/styles/main.css';
 
 export default function OwnerHome() {
   return (
     <div>
-      <h1>Owner Home</h1>
+      <h1 className='text-pink-200 font-display'>Owner Home</h1>
       <p>여기는 Owner(보호자) 홈 화면입니다.</p>
     </div>
   );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(), 
-    tsconfigPaths(), // tsconfig.json의 경로 설정을 Vite에서 사용할 수 있도록 해줌
+    tsconfigPaths(),
+    tailwindcss(), // tsconfig.json의 경로 설정을 Vite에서 사용할 수 있도록 해줌
   ],
   
 })
