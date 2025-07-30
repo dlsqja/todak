0a4127e5c6c7f7ff9a9ee96ff710017041cc390f
송인범 <dlsqja3436@gmail.com>
2025-07-30 09:11:22 +0900

    add: 모든 병원 찾기

--- backend/src/main/java/com/A409/backend/domain/home/controller/HomeController.java
+++ backend/src/main/java/com/A409/backend/domain/home/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.A409.backend.domain.home.controller;
 
+import com.A409.backend.domain.hospital.dto.HospitalResponse;
+import com.A409.backend.domain.hospital.service.HospitalService;
 import com.A409.backend.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/public")
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final HospitalService hospitalService;
     
     @GetMapping("/")
     public ApiResponse<?> home() {
@@ -17,7 +25,8 @@ public class HomeController {
     @GetMapping("/hospitals")
     public ApiResponse<?> getHospitals() {
 
-        return ApiResponse.ofSuccess("A409팀입니다 ㄱ-");
+        List<HospitalResponse> hospitals = hospitalService.getHospitals();
+        return ApiResponse.ofSuccess(hospitals);
     }
 
     @GetMapping("/hospitals/{hospital_id}/vets")
--- backend/src/main/java/com/A409/backend/domain/hospital/service/HospitalService.java
+++ backend/src/main/java/com/A409/backend/domain/hospital/service/HospitalService.java
@@ -1,8 +1,13 @@
 package com.A409.backend.domain.hospital.service;
 
+import com.A409.backend.domain.hospital.dto.HospitalResponse;
+import com.A409.backend.domain.hospital.entity.Hospital;
 import com.A409.backend.domain.hospital.repository.HospitalRepository;
+import com.A409.backend.domain.user.vet.repository.VetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +15,10 @@ public class HospitalService {
 
     private final HospitalRepository hospitalRepository;
 
-    public
+    public List<HospitalResponse> getHospitals(){
+
+        List<Hospital> hospitals = hospitalRepository.findAll();
+
+        return hospitals.stream().map(HospitalResponse::toResponse).toList();
+    }
 }
