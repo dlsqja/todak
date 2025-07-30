3c52e988a9146d04b885388b3f970b24d744765e
daeyeonlee <angellee98@naver.com>
2025-07-30 09:29:29 +0900

    feat : 기본 폰트 변경경
    - 기본 폰트 Pretendard Variable 적용

--- frontend/src/layouts/MobileLayout.tsx
+++ frontend/src/layouts/MobileLayout.tsx
@@ -6,28 +6,31 @@ interface MobileLayoutProps {
 
 export default function MobileLayout({ children }: MobileLayoutProps) {
   return (
-    <div style={{
-      minHeight: '100vh',
-      background: '#f8fafc',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <div style={{
-        width: '100%',
-        maxWidth: 430,
-        minHeight: '100vh',
-        background: '#fff',
-        boxShadow: '0 0 16px 0 rgba(0,0,0,0.08)',
-        borderRadius: 16,
-        padding: 24,
-        boxSizing: 'border-box',
-        display: 'flex',
-        flexDirection: 'column',
-      }}>
-        {children}
-      </div>
+    <div className='font-pretendard h-screen mx-auto max-w-md bg-green-100 relative flex flex-col items-center pb-16'>
+      {children}
     </div>
   );
 } 
 
+// <div style={{
+//   minHeight: '100vh',
+//   background: '#f8fafc',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+// }}>
+//   <div style={{
+//     width: '100%',
+//     maxWidth: 430,
+//     minHeight: '100vh',
+//     background: '#fff',
+//     boxShadow: '0 0 16px 0 rgba(0,0,0,0.08)',
+//     borderRadius: 16,
+//     padding: 24,
+//     boxSizing: 'border-box',
+//     display: 'flex',
+//     flexDirection: 'column',
+//   }}>
+//     {children}
+//   </div>
+// </div>
