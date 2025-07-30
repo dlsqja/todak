a86afe06501daa8a7dc2e0d271b6346a14eab429
송인범 <dlsqja3436@gmail.com>
2025-07-30 14:02:10 +0900

    add: owner reservation 기능 추가

---
+++ backend/src/main/java/com/A409/backend/domain/reservation/dto/ReservationResponse.java
@@ -0,0 +1,49 @@
+package com.A409.backend.domain.reservation.dto;
+
+import com.A409.backend.domain.hospital.entity.Hospital;
+import com.A409.backend.domain.pet.dto.PetResponse;
+import com.A409.backend.domain.pet.entity.Pet;
+import com.A409.backend.domain.reservation.entity.Reservation;
+import com.A409.backend.domain.user.owner.entity.Owner;
+import com.A409.backend.domain.user.owner.entity.OwnerResponse;
+import com.A409.backend.domain.user.vet.entity.Vet;
+import com.A409.backend.global.enums.Status;
+import com.A409.backend.global.enums.Subject;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class ReservationResponse {
+
+    private Long reservationId;
+    private OwnerResponse owner;
+    private PetResponse pet;
+    private String hospitalName;
+    private String vetName;
+    private LocalDate reservationDay;
+    private Byte reservationTime;
+    private String photo;
+    private String description;
+    private Subject subject;
+    private Status status;
+
+    public static ReservationResponse toOwnerResponse(Reservation reservation){
+        return ReservationResponse.builder()
+                .reservationId(reservation.getReservationId())
+                .pet(PetResponse.toResponse(reservation.getPet()))
+                .hospitalName(reservation.getHospital().getName())
+                .vetName(reservation.getVet().getName())
+                .reservationDay(reservation.getReservationDay())
+                .reservationTime(reservation.getReservationTime())
+                .photo(reservation.getPhoto())
+                .description(reservation.getDescription())
+                .subject(reservation.getSubject())
+                .status(reservation.getStatus())
+                .build();
+    }
+
+}
--- backend/src/main/java/com/A409/backend/domain/reservation/repository/ReservationRepository.java
+++ backend/src/main/java/com/A409/backend/domain/reservation/repository/ReservationRepository.java
@@ -3,5 +3,8 @@ package com.A409.backend.domain.reservation.repository;
 import com.A409.backend.domain.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findAllByOwner_OwnerId(Long ownerOwnerId);
 }
