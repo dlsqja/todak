9a0a436912b756b634baa2af86ec9437b1b9c0a2
Jingyeong Han <hanjingyeong97@gmail.com>
2025-07-31 17:56:26 +0900

    feat : state 컴포넌트 생성

--- frontend/src/component/card/TreatmentSlideCard.tsx
+++ frontend/src/component/card/TreatmentSlideCard.tsx
@@ -1,22 +1,13 @@
 import React from "react";
+import StatusBadge from "@/component/state/StatusBadge";
 
 interface TreatmentSlideCardProps {
   department: string;
   petName: string;
   petInfo: string;
   time: string;
-  isAuthorized: boolean; // ✅ 수정: 상태값 boolean 으로 받음
+  isAuthorized: boolean;
 }
-
-const getStatusStyle = (isAuthorized: boolean) => {
-  return isAuthorized
-    ? "bg-green-300 text-green-100"
-    : "bg-green-200 text-brown-300";
-};
-
-const getStatusText = (isAuthorized: boolean) => {
-  return isAuthorized ? "서명 완료" : "검토 대기";
-};
 
 const TreatmentSlideCard: React.FC<TreatmentSlideCardProps> = ({
   department,
@@ -38,13 +29,11 @@ const TreatmentSlideCard: React.FC<TreatmentSlideCardProps> = ({
         <p className="p text-black">{petName}</p>
 
         {/* 하단: 반려동물 정보 + 상태 뱃지 */}
-        <div className="flex justify-between items-baseline">
-          <p className="caption text-black">{petInfo}</p>
-          <span
-            className={`caption px-3 py-1 rounded-full ${getStatusStyle(isAuthorized)}`}
-          >
-            {getStatusText(isAuthorized)}
-          </span>
+        <div className="flex justify-between items-end">
+          <p className="caption text-black leading-none">{petInfo}</p>
+          <div className="flex-shrink-0 leading-none translate-y-[-4px]">
+            <StatusBadge isActive={isAuthorized} type="treatment" />
+          </div>
         </div>
       </div>
     </div>
