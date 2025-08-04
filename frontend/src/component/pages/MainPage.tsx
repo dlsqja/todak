16328b20ac9ef4c65cfd3b24344890c2df7bbfe9
daeyeonlee <angellee98@naver.com>
2025-08-04 20:09:32 +0900

    feat : 메인 페이지 추가
    - 메인 페이지 레이아웃 추가
    - 라우터 연결
    - 기타 아이콘 및 CSS 추가가

--- frontend/src/component/pages/MainPage.tsx
+++ frontend/src/component/pages/MainPage.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import OwnerMenuBar from '@/component/menubar/OwnerMenuBar';
 
 function HomePage() {
-    return (
-        <div>
-            <h1 className='h1'>Home Page </h1>
-            <br />
-            <h3 className='h3'> 여기는 홈 화면입니다.</h3>
-            <br />
-            <ul>
-                <li className='underline'><Link to="/owner/home">Owner Home 이동</Link></li>
-                <li className='underline'><Link to="/vet/home">Vet Home 이동</Link></li>
-                <li className='underline'><Link to="/staff/home">Staff Home 이동</Link></li>
-                <li className='underline'><Link to="/role-redirect">Role Redirect 테스트</Link></li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="h3">역할 선택 페이지</h3>
+      <br />
+      <ul>
+        <li className="underline">
+          <Link to="/owner/home">Owner Home 이동</Link>
+        </li>
+        <li className="underline">
+          <Link to="/vet/home">Vet Home 이동</Link>
+        </li>
+        <li className="underline">
+          <Link to="/staff/home">Staff Home 이동</Link>
+        </li>
+        <li className="underline">
+          <Link to="/role-redirect">Role Redirect 테스트</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ frontend/src/router/ownerRoutes.tsx
@@ -0,0 +1,21 @@
+import MobileLayout from '@/layouts/MobileLayout';
+import OwnerMenuBar from '@/component/menubar/OwnerMenuBar';
+import OwnerHome from '@/component/pages/Owner/OwnerHome';
+import OwnerAnimal from '@/component/pages/Owner/OwnerAnimal';
+import OwnerMyPage from '@/component/pages/Owner/OwnerMyPage';
+import OwnerReservation from '@/component/pages/Owner/OwnerReservation';
+import OwnerTreatment from '@/component/pages/Owner/OwnerTreatment';
+
+const ownerRoutes = {
+  path: '/owner',
+  element: <MobileLayout menuBar={<OwnerMenuBar />} />,
+  children: [
+    { path: 'home', element: <OwnerHome /> },
+    { path: 'animal', element: <OwnerAnimal /> },
+    { path: 'reservation', element: <OwnerReservation /> },
+    { path: 'treatment', element: <OwnerTreatment /> },
+    { path: 'mypage', element: <OwnerMyPage /> },
+  ],
+};
+
+export default ownerRoutes;
