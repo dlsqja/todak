27be0448d1ff5c31f44e9956191d6375bf473645
daeyeonlee <angellee98@naver.com>
2025-08-04 12:32:06 +0900

    refact : 복사 버튼 수정
    - 클립보드에 복사되도록 기능 수정

--- frontend/src/component/button/CopyButton.tsx
+++ frontend/src/component/button/CopyButton.tsx
@@ -1,7 +1,17 @@
-
 export default function CopyButton() {
+  const animalCode = 'A409'; //  임시로 고정(실제 동물코드 값 입력력)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(animalCode);
+    alert('동물코드가 복사되었습니다!'); // 안내 메시지
+  };
+
   return (
-    <button className={`w-14 h-6 rounded-[12px] h5 bg-gray-100 text-gray-500 `}>
+    <button
+      className="w-14 h-6 rounded-[12px] h5 bg-gray-100 text-gray-500 cursor-pointer"
+      onClick={handleCopy}
+      type="button"
+    >
       복사
     </button>
   );
