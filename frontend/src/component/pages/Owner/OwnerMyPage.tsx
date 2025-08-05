d550a766e2b17037de2f395049775fcd148830d7
daeyeonlee <angellee98@naver.com>
2025-08-05 19:50:13 +0900

    feat : 마이페이지 페이지 추가

--- frontend/src/component/pages/Owner/OwnerMyPage.tsx
+++ frontend/src/component/pages/Owner/OwnerMyPage.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import '@/styles/main.css';
+import BackHeader from '@/component/header/BackHeader';
+import Input from '@/component/input/Input';
 
-export default function OwnerHome() {
+export default function OwnerMyPage() {
   return (
-    <div>
-      <h1 className="h1">Owner My Page</h1>
-      <p>여기는 반려인 마이페이지 화면입니다.</p>
-    </div>
+    <>
+      <BackHeader text="마이페이지" />
+      <div className="flex flex-col gap-6 px-4 mt-11">
+        <Input id="name" label="이름" value="김싸피" disabled={true} />
+        <Input id="birth" label="생년월일" value="1998.12.23" disabled={true} />
+        <Input id="phone" label="전화번호" value="010-3953-8888" disabled={true} />
+      </div>
+      <br />
+      <div className="flex justify-center gap-2">
+        <button className="h4 text-center text-gray-500 cursor-pointer">로그 아웃</button>
+        <span className="text-gray-500"> | </span>
+        <button className="h4 text-center text-gray-500 cursor-pointer">회원 탈퇴</button>
+      </div>
+    </>
   );
 }
