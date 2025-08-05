6ba606887bb7c21292a2d7446a69406c1e6e790e
송인범 <dlsqja3436@gmail.com>
2025-08-05 14:43:25 +0900

    feat: service 추가

--- backend/src/main/java/com/A409/backend/domain/reservation/controller/StaffReservationController.java
+++ backend/src/main/java/com/A409/backend/domain/reservation/controller/StaffReservationController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 public class StaffReservationController {
-
     @GetMapping
     public APIResponse<?> getAllHospitalReservations(@AuthenticationPrincipal User user){
         return null;
