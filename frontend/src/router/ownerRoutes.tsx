9fcb4c2f4470e56fed222444f1ba8ebcfb9468f3
daeyeonlee <angellee98@naver.com>
2025-08-05 16:05:50 +0900

    feat : 예약 상세 페이지 추가

--- frontend/src/router/ownerRoutes.tsx
+++ frontend/src/router/ownerRoutes.tsx
@@ -3,10 +3,11 @@ import OwnerMenuBar from '@/component/menubar/OwnerMenuBar';
 import OwnerHome from '@/component/pages/Owner/OwnerHome';
 import OwnerPet from '@/component/pages/Owner/Pet/OwnerPetHome';
 import OwnerMyPage from '@/component/pages/Owner/OwnerMyPage';
-import OwnerReservation from '@/component/pages/Owner/Reservation/OwnerReservationHome';
+import OwnerReservationHome from '@/component/pages/Owner/Reservation/OwnerReservationHome';
 import OwnerTreatment from '@/component/pages/Owner/OwnerTreatment';
 import OwnerPetEdit from '@/component/pages/Owner/Pet/OwnerPetEdit';
 import OwnerPetRegister from '@/component/pages/Owner/Pet/OwnerPetRegister';
+import OwnerReservationDetail from '@/component/pages/Owner/Reservation/OwnerReservationDetail';
 
 const ownerRoutes = {
   path: '/owner',
@@ -16,7 +17,8 @@ const ownerRoutes = {
     { path: 'pet', element: <OwnerPet /> },
     { path: 'pet/register', element: <OwnerPetRegister /> },
     { path: 'pet/edit/:id', element: <OwnerPetEdit /> },
-    { path: 'reservation', element: <OwnerReservation /> },
+    { path: 'reservation', element: <OwnerReservationHome /> },
+    { path: 'reservation/:id', element: <OwnerReservationDetail /> },
     { path: 'treatment', element: <OwnerTreatment /> },
     { path: 'mypage', element: <OwnerMyPage /> },
   ],
