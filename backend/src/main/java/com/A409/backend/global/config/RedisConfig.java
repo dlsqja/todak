19cbde8251bb5e5ab15d203f9feba8a54bfb698a
송인범 <dlsqja3436@gmail.com>
2025-08-05 15:30:12 +0900

    fix: 로컬 개발용 수정

--- backend/src/main/java/com/A409/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/A409/backend/global/config/RedisConfig.java
@@ -17,6 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class RedisConfig {
 
+    /*
     //배포용 센티널 설정
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -29,6 +30,7 @@ public class RedisConfig {
 
         return new LettuceConnectionFactory(sentinelConfig);
     }
+     */
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
