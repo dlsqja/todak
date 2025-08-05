da2fbac5714cea4567c50ce1ab0c4cc3f6fcb3b4
JYJ <yunzly@gmail.com>
2025-08-05 12:50:37 +0900

    docs : Animal -> Pet
    - 'Animal'이라는 표현을 'Pet'으로 교체

--- frontend/src/router/ownerRoutes.tsx
+++ frontend/src/router/ownerRoutes.tsx
@@ -1,17 +1,19 @@
 import MobileLayout from '@/layouts/MobileLayout';
 import OwnerMenuBar from '@/component/menubar/OwnerMenuBar';
 import OwnerHome from '@/component/pages/Owner/OwnerHome';
-import OwnerAnimal from '@/component/pages/Owner/OwnerAnimal';
+import OwnerPet from '@/component/pages/Owner/Pet/OwnerPet';
 import OwnerMyPage from '@/component/pages/Owner/OwnerMyPage';
 import OwnerReservation from '@/component/pages/Owner/OwnerReservation';
 import OwnerTreatment from '@/component/pages/Owner/OwnerTreatment';
+import RegisterPet from '@/component/pages/Owner/Pet/RegisterPet';
 
 const ownerRoutes = {
   path: '/owner',
   element: <MobileLayout menuBar={<OwnerMenuBar />} />,
   children: [
     { path: 'home', element: <OwnerHome /> },
-    { path: 'animal', element: <OwnerAnimal /> },
+    { path: 'pet', element: <OwnerPet /> },
+    { path: 'pet/register', element: <RegisterPet /> },
     { path: 'reservation', element: <OwnerReservation /> },
     { path: 'treatment', element: <OwnerTreatment /> },
     { path: 'mypage', element: <OwnerMyPage /> },
