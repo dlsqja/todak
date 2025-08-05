eebc95168426d30c2a126643fb4eedcc40aee3b6
송인범 <dlsqja3436@gmail.com>
2025-08-05 14:38:54 +0900

    feat: add vet dto

---
+++ backend/src/main/java/com/A409/backend/domain/reservation/dto/ReservationResponseToVet.java
@@ -0,0 +1,46 @@
+package com.A409.backend.domain.reservation.dto;
+
+import com.A409.backend.domain.pet.dto.PetResponse;
+import com.A409.backend.domain.reservation.entity.Reservation;
+import com.A409.backend.domain.user.owner.entity.OwnerResponse;
+import com.A409.backend.global.enums.Status;
+import com.A409.backend.global.enums.Subject;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class ReservationResponseToVet {
+
+    private Long reservationId;
+    private OwnerResponse owner;
+    private PetResponse pet;
+    private String hospitalName;
+    private String vetName;
+    private LocalDate reservationDay;
+    private Byte reservationTime;
+    private String photo;
+    private String description;
+    private Subject subject;
+    private Status status;
+
+    public static ReservationResponseToVet toOwnerResponse(Reservation reservation){
+        return ReservationResponseToVet.builder()
+                .reservationId(reservation.getReservationId())
+                .owner(OwnerResponse.toResponse(reservation.getOwner()))
+                .pet(PetResponse.toResponse(reservation.getPet()))
+                .hospitalName(reservation.getHospital().getName())
+                .vetName(reservation.getVet().getName())
+                .reservationDay(reservation.getReservationDay())
+                .reservationTime(reservation.getReservationTime())
+                .photo(reservation.getPhoto())
+                .description(reservation.getDescription())
+                .subject(reservation.getSubject())
+                .status(reservation.getStatus())
+                .build();
+    }
+
+}
+
