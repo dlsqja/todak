42dc75805ef691611c1665f52b05fde87389ad43
daeyeonlee <angellee98@naver.com>
2025-08-06 12:47:10 +0900

    feat : 수의사 홈 화면 추가가
    - 비대면 진료 예정 목록 carosaul 구현
    - 진료 기록 검토 scroll 구현

--- frontend/src/component/pages/Vet/VetHome.tsx
+++ frontend/src/component/pages/Vet/VetHome.tsx
@@ -1,59 +1,158 @@
-import { useState } from 'react';
-import BackHeader from '@/component/header/BackHeader';
-import Button from '@/component/button/Button';
-import CopyButton from '@/component/button/CopyButton';
-import Input from '@/component/input/Input';
-import TimeSelectionButton from '@/component/selection/TimeSelectionButton';
-import TimeSelectionDropdown from '@/component/selection/TimeSelectionDropdown';
-import ImageInputBox from '@/component/input/ImageInputBox';
-import SelectionDropdown from '@/component/selection/SelectionDropdown';
-import FilteredList from '@/component/selection/FilterDropdown';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '@/styles/main.css';
+import OwnerTreatmentSimpleCard from '@/component/card/OwnerTreatmentSimpleCard';
+import TreatmentSlideCard from '@/component/card/TreatmentSlideCard';
+import TreatmentSlideList from '@/component/card/TreatmentSlideList';
+
+interface CompletedReservation {
+  reservation_id: number;
+  owner_id: number;
+  pet_id: number;
+  hospital_id: number;
+  vet_id: number;
+  reservation_day: string;
+  reservation_time: number;
+  photo: string;
+  description: string;
+  subject: string;
+  status: string;
+  petName: string;
+  petInfo: string;
+}
 
 export default function VetHome() {
-  const [name, setName] = useState('');
-  const [birthday, setBirthday] = useState('');
+  const navigate = useNavigate();
+  const timeMapping: { [key: number]: string } = {
+    0: '00:00', // 자정
+    1: '00:30',
+    2: '01:00',
+    3: '01:30',
+    4: '02:00',
+    5: '02:30',
+    6: '03:00',
+    7: '03:30',
+    8: '04:00',
+    9: '04:30',
+    10: '05:00',
+    11: '05:30',
+    12: '06:00',
+    13: '06:30',
+    14: '07:00',
+    15: '07:30',
+    16: '08:00',
+    17: '08:30',
+    18: '09:00',
+    19: '09:30',
+    20: '10:00',
+    21: '10:30',
+    22: '11:00',
+    23: '11:30',
+    24: '12:00', // 정오
+    25: '12:30',
+    26: '13:00',
+    27: '13:30',
+    28: '14:00',
+    29: '14:30',
+    30: '15:00',
+    31: '15:30',
+    32: '16:00',
+    33: '16:30',
+    34: '17:00',
+    35: '17:30',
+    36: '18:00',
+    37: '18:30',
+    38: '19:00',
+    39: '19:30',
+    40: '20:00',
+    41: '20:30',
+    42: '21:00',
+    43: '21:30',
+    44: '22:00',
+    45: '22:30',
+    46: '23:00',
+    47: '23:30', // 자정 직전
+  };
 
-  // 테스트 : 필터링 옵션
-  const options1 = [
-    { value: '0', label: '치과' },
-    { value: '1', label: '피부과' },
-    { value: '2', label: '골절' },
-    { value: '3', label: '안과' },
-  ];
-  const options2 = [
-    { value: 'all', label: '전체' },
-    { value: 'cat', label: '고양이' },
-    { value: 'dog', label: '강아지' },
+  // 더미데이터
+  const reservations: CompletedReservation[] = [
+    {
+      reservation_id: 1,
+      owner_id: 1,
+      pet_id: 1,
+      hospital_id: 1,
+      vet_id: 1,
+      reservation_day: '2025-01-15',
+      reservation_time: 34, // 17:00
+      photo: '/images/미료_test.jpg',
+      description: '피부에 발진이 생기고 가려움을 호소합니다. 진료 후 약을 처방받았습니다.',
+      subject: '피부과',
+      status: '완료',
+      petName: '미료',
+      petInfo: '고양이 / 2세 / 여(중성화)',
+    },
+    {
+      reservation_id: 2,
+      owner_id: 1,
+      pet_id: 2,
+      hospital_id: 1,
+      vet_id: 2,
+      reservation_day: '2025-01-14',
+      reservation_time: 29, // 14:30
+      photo: '/images/미료 _test.jpg',
+      description: '식욕이 없고 설사를 하고 있습니다. 소화제를 처방받았습니다.',
+      subject: '치과',
+      status: '완료',
+      petName: '구름이',
+      petInfo: '강아지 / 5세 / 남(중성화)',
+    },
+    {
+      reservation_id: 3,
+      owner_id: 1,
+      pet_id: 2,
+      hospital_id: 1,
+      vet_id: 2,
+      reservation_day: '2025-01-14',
+      reservation_time: 29, // 14:30
+      photo: '/images/미료 _test.jpg',
+      description: '식욕이 없고 설사를 하고 있습니다. 소화제를 처방받았습니다.',
+      subject: '치과',
+      status: '완료',
+      petName: '구름이',
+      petInfo: '강아지 / 5세 / 남(중성화)',
+    },
   ];
 
   return (
-    <div className="mx-7">
-      <BackHeader text="홈으로 돌아가기" />
-      <h1>Vet Home</h1>
-      <p>여기는 Vet(수의사)홈 화면입니다.</p>
-      <Button color="lightgreen" text="요약본 정리하기" />
-      <Button color="green" text="요약본 정리하기" />
-      <Button color="pink" text="요약본 정리하기" />
-      <CopyButton />
-      <Input
-        id="name"
-        label="이름"
-        placeholder="이름을 입력하세요"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        id="birthday"
-        label="생년월일"
-        placeholder="생년월일을 입력하세요"
-        value={birthday}
-        onChange={(e) => setBirthday(e.target.value)}
-      />
-      <TimeSelectionButton start_time="08:00" end_time="21:00" />
-      <TimeSelectionDropdown start_time="08:00" end_time="21:00" label="진료 가능 시간" />
-      <ImageInputBox src="/images/미료_test.jpg" />
-      <SelectionDropdown options={options1} placeholder="진료 과목을 선택하세요" />
-      <FilteredList options={options2} />
+    <div>
+      <h3 className="h3 mx-7 pt-13">OOO수의사님 반갑습니다!</h3>
+      <h3 className="h3 mx-7 mb-2">어플 사용이 처음이신가요?</h3>
+      <button
+        onClick={() => navigate('/guide')}
+        className="h5 mx-7 px-5 py-1 rounded-full inline-block 
+        bg-green-300 text-green-100 hover:bg-green-200 transition"
+      >
+        비대면 진료 가이드
+      </button>
+      <h3 className="mx-7 h3 mt-11">비대면 진료 예정 목록</h3>
+      <div className="overflow-x-auto overflow-visible snap-x snap-mandatory scroll-smooth hide-scrollbar mx-7 pt-3 pb-6">
+        <div className="w-full overflow-x-auto hide-scrollbar flex gap-7 h-full p-3">
+          {reservations.map((reservation) => (
+            <div key={reservation.reservation_id}>
+              <OwnerTreatmentSimpleCard
+                time={timeMapping[reservation.reservation_time]}
+                department={reservation.subject}
+                petName={reservation.petName}
+                petInfo={reservation.petInfo}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <h3 className="mx-7 h3">진료 기록 검토</h3>
+      <div className="mx-7 ">
+        <TreatmentSlideList />
+      </div>
     </div>
   );
 }
--- frontend/src/component/state/StatusBadge.tsx
+++ frontend/src/component/state/StatusBadge.tsx
@@ -1,58 +1,54 @@
-import React from "react";
+import React from 'react';
 
 interface StatusBadgeProps {
-  type: "treatment" | "reservation" | "payment" ;
+  type: 'treatment' | 'reservation' | 'payment';
   statusKey: string | number; // 0, 1, 2, "success", "fail" 등
 }
-
 
 const statusConfig = {
   treatment: {
     true: {
-      text: "서명 완료",
-      className: "bg-green-300 text-green-100",
+      text: '서명 완료',
+      className: 'bg-green-300 text-green-100',
     },
     false: {
-      text: "검토 대기",
-      className: "bg-green-200 text-brown-300",
+      text: '검토 대기',
+      className: 'bg-green-200 text-brown-300',
     },
   },
-    reservation: {
+  reservation: {
     0: {
-      text: "대기",
-      className: "bg-green-200 text-brown-300",
+      text: '대기',
+      className: 'bg-green-200 text-brown-300',
     },
     1: {
-      text: "승인",
-      className: "bg-green-300 text-green-100",
+      text: '승인',
+      className: 'bg-green-300 text-green-100',
     },
     2: {
-      text: "반려",
-      className: "bg-pink-200 text-green-100",
+      text: '반려',
+      className: 'bg-pink-200 text-green-100',
     },
   },
   payment: {
     true: {
-      text: "결제 완료",
-      className: "bg-green-300 text-green-100",
+      text: '결제 완료',
+      className: 'bg-green-300 text-green-100',
     },
     false: {
-      text: "미결제",
-      className: "bg-green-200 text-brown-300",
+      text: '미결제',
+      className: 'bg-green-200 text-brown-300',
     },
   },
-
 };
 
 const StatusBadge: React.FC<StatusBadgeProps> = ({ statusKey, type }) => {
-  const config = statusConfig[type][String(statusKey)];
+  const config = statusConfig[type]?.[String(statusKey)] || {
+    text: '알 수 없음',
+    className: 'bg-gray-200 text-gray-600',
+  };
 
-  return (
-    <span className={`caption px-3 py-1 rounded-full ${config.className}`}>
-      {config.text}
-    </span>
-  );
+  return <span className={`caption px-3 py-1 rounded-full ${config.className}`}>{config.text}</span>;
 };
-
 
 export default StatusBadge;
