002979730a083d67cf38c4c3d63c8b2a32fcca3b
daeyeonlee <angellee98@naver.com>
2025-08-06 17:39:47 +0900

    refact : treat navar 컴포넌트 수정

--- frontend/src/component/navbar/TabGroupTreatList.tsx
+++ frontend/src/component/navbar/TabGroupTreatList.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
-  selected: string;
   onSelect: (tab: string) => void;
 }
 
-export default function TabGroupTreatList({ selected, onSelect }: Props) {
+export default function TabGroupTreatList({ onSelect }: Props) {
+  const [selected, setSelected] = useState('목록형');
   const tabs = ['목록형', '날짜형'];
   return (
-    <div className="flex justify-center gap-15">
+    <div className="px-5 w-full flex justify-center gap-12 border-b-2 border-gray-100">
       {tabs.map((tab) => (
         <button
           key={tab}
-          onClick={() => onSelect(tab)}
-          className={`h4 ${
-            selected === tab ? 'h4 text-center border-b-2 border-black' : 'h4 text-center text-gray-500'
+          onClick={() => {
+            onSelect(tab);
+            setSelected(tab);
+          }}
+          className={`pb-2 h4 cursor-pointerD ${
+            selected === tab ? 'h4 text-black border-b-2 border-black' : 'h4 text-center text-gray-500'
           }`}
         >
           {tab}
