5e191b7c41ed59fde222e5616ac603f95213135c
daeyeonlee <angellee98@naver.com>
2025-08-14 17:18:56 +0900

    style : 병원 목록 스타일 수정

--- frontend/src/component/pages/Owner/Home/OwnerHomeSelectHospital.tsx
+++ frontend/src/component/pages/Owner/Home/OwnerHomeSelectHospital.tsx
@@ -341,10 +341,8 @@ export default function SelectHospitalPage() {
 
         {/* 검색어가 없을 때 안내 메시지 */}
         {!search.trim() && (
-          <div className="mt-8">
-            <div className="bg-gray-50 rounded-xl p-8 text-center">
-              <p className="text-gray-400 p">병원명을 검색해주세요.</p>
-            </div>
+          <div className="bg-gray-50 rounded-xl p-4 text-center">
+            <p className="text-gray-400 p">병원명 혹은 병원 주소를 검색해주세요.</p>
           </div>
         )}
       </div>
