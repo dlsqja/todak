c73b051f0ebad580edd4e6c9172c48a42211448e
daeyeonlee <angellee98@naver.com>
2025-08-14 13:01:36 +0900

    refact : 레이아웃 세로 길이 수정

--- frontend/src/layouts/MobileAuthLayout.tsx
+++ frontend/src/layouts/MobileAuthLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const MobileAuthLayout = () => (
   <div className="max-h-screen mx-auto max-w-md bg-gray-50 flex flex-col pt-7">
-    <main className="h-screen pb-2">
+    <main className="h-screen overflow-y-auto pb-2">
       <Outlet />
     </main>
   </div>
