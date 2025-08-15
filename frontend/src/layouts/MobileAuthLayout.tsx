280af8b6700f1b0cb37d73748b140ca1f68380a6
daeyeonlee <angellee98@naver.com>
2025-08-15 23:53:18 +0900

    style : 레이아웃 스타일 변경
    - 스크롤바 삭제

--- frontend/src/layouts/MobileAuthLayout.tsx
+++ frontend/src/layouts/MobileAuthLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const MobileAuthLayout = () => (
   <div className="max-h-screen bg-gray-100 flex justify-center items-start">
     <div className="w-full mx-auto max-w-md bg-gray-50 flex flex-col pt-7 shadow-lg">
-      <main className="h-screen overflow-y-auto pb-2">
+      <main className="h-screen overflow-y-auto pb-2 hide-scrollbar">
         <Outlet />
       </main>
     </div>
--- frontend/src/layouts/MobileLayout.tsx
+++ frontend/src/layouts/MobileLayout.tsx
@@ -8,8 +8,7 @@ interface MobileLayoutProps {
 const MobileLayout: React.FC<MobileLayoutProps> = ({ menuBar }) => (
   <div className="max-h-screen bg-gray-100 flex justify-center items-start">
     <div id="app-shell" className="max-h-screen w-full mx-auto max-w-md bg-gray-50 flex flex-col pt-7 shadow-lg">
-      <main className="h-screen overflow-y-auto pb-2">
-        {/* hidden scrollbar  설정가능*/}
+      <main className="h-screen overflow-y-auto pb-2 hide-scrollbar">
         <Outlet />
       </main>
       {menuBar}
