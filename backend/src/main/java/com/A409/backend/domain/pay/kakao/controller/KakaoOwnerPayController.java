c0825987e18bacd69202e200026320586c888790
김유성 <usk05@daum.net>
2025-08-15 23:45:30 +0900

    refact: owener pay, staff pay url 수정(security 맞게)

--- backend/src/main/java/com/A409/backend/domain/pay/kakao/controller/KakaoOwnerPayController.java
+++ backend/src/main/java/com/A409/backend/domain/pay/kakao/controller/KakaoOwnerPayController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/owner/pay")
+@RequestMapping("/owners/pay")
 @RequiredArgsConstructor
 public class KakaoOwnerPayController {
 
--- backend/src/main/java/com/A409/backend/domain/pay/kakao/controller/KakaoStaffPayController.java
+++ backend/src/main/java/com/A409/backend/domain/pay/kakao/controller/KakaoStaffPayController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/staff/pay")
+@RequestMapping("/staffs/pay")
 @RequiredArgsConstructor
 public class KakaoStaffPayController {
 
